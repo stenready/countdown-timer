cb6958c6ff9511df6560654f1c163d6ab0685d99
stenready <44876507+stenready@users.noreply.github.com>
2020-07-13 14:32:11 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+timer(
+  '2020-12-10',
+  {
+    days: '.days',
+    daysTitle: '.days_title',
+  
+    hours: '.hours',
+    hoursTitle: '.hours_title',
+  
+    minutes: '.minutes',
+    minutesTitle: '.minutes_title',
+  
+    seconds: '.seconds',
+    secondsTitle: '.seconds_title',
+  
+  }
+)
